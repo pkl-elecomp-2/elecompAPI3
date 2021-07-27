bb6e10e1c07ecac7291c5cc2b2611a60813bdca8
Unero <20809454+un-ro@users.noreply.github.com>
2021-07-27 09:15:47 +0700

    :shipit: Beranda

---
+++ application/controllers/Beranda.php
@@ -0,0 +1,40 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+use chriskacerguis\RestServer\RESTController;
+
+class Beranda extends RESTController {
+    
+    function __construct($config = 'rest'){
+        parent::__construct($config);
+        $this->load->database();
+    }
+
+    // GET /Beranda
+    function index_get(){
+        $data = array(
+            'data' => $this->db->get('tb_slider')->result()
+        );
+
+        $this->response($data, 200);
+    }
+
+    // GET /Beranda/layanan
+    function layanan_get(){
+        $data = array(
+            'data' => $this->db->get('tb_jenis_layanan')->result()
+        );
+
+        $this->response($data, 200);
+    }
+
+    // GET /Beranda/promo
+    function promo_get(){
+        $data = array(
+            'data' => $this->db->get('tb_promo')->result()
+        );
+
+        $this->response($data, 200);
+    }
+}
